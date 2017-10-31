1338ceaffc9236281f43103ad87d6639a2f355e8
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2017-10-31 10:36:41 -0500

    assignment 10 Python Scores and Grades

---
+++ Rodolfo_Valdivieso/PythonFundamentals/assignment10PythonScores.py
@@ -0,0 +1,17 @@
+#Assignment 10 Python Scores and Grades
+#function that generates ten scores between 60 and 100
+import random
+
+for i in range(1, 11):
+	score = random.randint(60,100)
+
+	if (score <= 69):
+		print "Score: ", score , " Your grade is D"
+	elif (score >= 70 and score <= 79):
+		print "Score: ", score , " Your grade is C"
+	elif (score >= 80 and score <= 89):
+		print "Score: ", score , " Your grade is B"
+	else:
+		print "Score: ", score , " Your grade is A"
+
+print "End of the program. Bye!"
