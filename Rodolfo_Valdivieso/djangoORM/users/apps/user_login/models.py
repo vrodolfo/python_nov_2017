51e975985b6893ff2abe28356ccf1203700188a5
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2017-11-15 12:28:12 -0600

    Assignment Django ORM User

---
+++ Rodolfo_Valdivieso/djangoORM/users/apps/user_login/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+	first_name    = models.CharField(max_length=255)
+	last_name     = models.CharField(max_length=255)
+	email_address = models.CharField(max_length=255)
+	age           = models.IntegerField()
+	created_at    = models.DateTimeField(auto_now_add = True)
+	updated_at    = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return "first: " + str(self.first_name) + ", last: " + self.last_name + ", email: " + self.email_address
